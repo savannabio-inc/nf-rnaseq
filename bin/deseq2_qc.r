af4d5fd830b369c9e6a5bd4f74db02f2c5d652cd
Andrew Phan <aqph222@gmail.com>
2026-01-24 11:07:18 -0800

    Changes linewidth parameter to size for backwards compatibility

--- bin/deseq2_qc.r
+++ bin/deseq2_qc.r
@@ -312,7 +312,7 @@ for (n_top_var in ntop) {
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
             panel.background = element_blank(),
-            panel.border = element_rect(colour = "black", fill=NA, linewidth=1))
+            panel.border = element_rect(colour = "black", fill=NA, size=1))
     print(pl)
 
     if (decompose) {
